b85c15a7a1f5d222f1b3087f9ef29ae35eed4d2d
mbkma <johannes.unruh@fau.de>
2021-03-12 22:02:20 +0100

    synctex: use gi.require_version()

--- plugins/synctex/synctex/__init__.py
+++ plugins/synctex/synctex/__init__.py
@@ -1,1 +1,7 @@
+import gi
+gi.require_version('Pluma', '1.0')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Peas', '1.0')
+gi.require_version('PeasGtk', '1.0')
+
 from .synctex import SynctexWindowActivatable
