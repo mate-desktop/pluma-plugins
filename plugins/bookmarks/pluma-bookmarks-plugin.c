de1097141deb9c9ba15418ab0d265cd47053fe5c
zhuyaliang <15132211195@163.com>
2023-10-11 11:45:30 +0800

    add meson build support

--- plugins/bookmarks/pluma-bookmarks-plugin.c
+++ plugins/bookmarks/pluma-bookmarks-plugin.c
@@ -19,10 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif
-
 #include "pluma-bookmarks-plugin.h"
 
 #include <stdlib.h>
